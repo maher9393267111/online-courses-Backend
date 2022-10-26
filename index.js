69fa046d03e4857d9d978ea13232b2589461b0fa
maher <gomemahero.com>
2022-10-27 00:54:36 +0300

    add sequlize api to backend

--- index.js
+++ index.js
@@ -2,10 +2,104 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 6000;
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
 app.use(cors());
+app.use(express.json({ extended: true }));
 const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
+
+
+
+
+const option = {
+    host: "mysql-93288-0.cloudclusters.net",
+    //"sql11.freesqldatabase.com",
+    dialect: "mysql",
+    port: 10016,
+  //   pool: {
+  //     max: 30,
+  //     min: 0,
+  //   },
+  };
+  
+  const databaseName = "myFIRST";
+  const databaseUsername = "admin";
+  const databasePassword = "xC8D2L3M";
+  
+  const sequelize = new Sequelize(
+    databaseName,
+    databaseUsername,
+    databasePassword,
+    option
+  );
+  try {
+   // await 
+    sequelize.authenticate();
+    console.log("connection successful");
+  } catch (error) {
+    console.log(error , 'ERROR TO CONNECT TO DATABASE');
+  }
+
+
+
+
+
+  class Blogs extends Model {}
+
+  Blogs.init({
+      id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      author: {
+          type: DataTypes.STRING,
+      },
+      url: {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      title: {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      descrep: {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      likes: {
+          type: DataTypes.INTEGER,
+          default: 0
+      }
+  }, {
+      sequelize
+  })
+  
+  Blogs.sync({alter: true })  // update columns in sameTime
+  
+
+
+
+
+
+  app.get('/api/blogs', async(req, res) => {
+    try {
+        const blogs = await Blogs.findAll()
+        console.log(JSON.stringify(blogs, null, 2))
+        res.status(200).json(blogs )
+    } catch(err) {
+        console.log(err)
+        res.status(400).json({ msg: err })
+    }
+})
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Courses API is Running');
